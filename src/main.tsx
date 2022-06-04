b9e7fb43fab2b1bb1e8de4171dfee5bce31ef4e7
einfachIrgendwer0815 <85333734+einfachIrgendwer0815@users.noreply.github.com>
2022-06-04 11:56:46 +0200

    Switch from bare react to react with vite
    Include vite and rename .js files to .tsx

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+
+import SintroduceEditor from './components/sintroduceEditor/sintroduceEditor';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <SintroduceEditor />
+  </React.StrictMode>
+)
